d4e0a59667b89d50bf1be97f6f310c035b9f5f8f
Devansh Bhagania <devanshbhagania19@gmail.com>
2024-09-11 22:24:06 +0530

    feat: new routes added to the add file

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+const bookRoutes = require('./routes/bookRoutes');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+connectDB();
+
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/books', bookRoutes);
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
